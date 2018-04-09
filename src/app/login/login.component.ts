f7bdce977f17fcf997356a525fb58e47385c2fa1
lgmagalhaes88 <lgmagalhaes88@gmail.com>
2018-04-08 23:39:19 -0300

    Ajustado dela de login #10, menu de navegação #14 e criado versionamento 0.0.1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'cm-login',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  @Input() showHeader: boolean = true;
 
   constructor() { }
 
