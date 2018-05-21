09615bf5977b16b282af1c24968a533be5975b4a
Lucas Gomes <lucasgsm88@gmail.com>
2018-05-20 23:01:00 -0300

    criado construtor do grafo

--- Class-Management-System/Models/Structures/Grafo.cs
+++ Class-Management-System/Models/Structures/Grafo.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace Class_Management_System.Models.Structures
 {
     public class Grafo
     {
         private List<Vertice> vertices;
+
+        public Grafo()
+        {
+            this.vertices = new List<Vertice>();
+        }
     }
 }
