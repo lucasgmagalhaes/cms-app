5f3aeae4ea95c88f8c1909b83f1da472baebc6db
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-05-24 00:33:58 -0300

    Adicionado form CadUsuario
    Onde será feito o cadastro do usuário e consulta de dados de um usuário existente

---
+++ Class-Management-System/Forms/CadUsuario.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Class_Management_System.Forms
+{
+    partial class CadUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.txtEmailuser = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.cmbPerfil = new System.Windows.Forms.ComboBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblConfSenha = new System.Windows.Forms.Label();
+            this.lblPerfil = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.MaskedTextBox();
+            this.txtSenha = new System.Windows.Forms.MaskedTextBox();
+            this.txtConfirma = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Location = new System.Drawing.Point(82, 110);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(268, 29);
+            this.TxtNome.TabIndex = 0;
+            // 
+            // txtEmailuser
+            // 
+            this.txtEmailuser.Location = new System.Drawing.Point(434, 205);
+            this.txtEmailuser.Name = "txtEmailuser";
+            this.txtEmailuser.Size = new System.Drawing.Size(262, 29);
+            this.txtEmailuser.TabIndex = 1;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(82, 205);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(268, 29);
+            this.txtLogin.TabIndex = 4;
+            // 
+            // cmbPerfil
+            // 
+            this.cmbPerfil.FormattingEnabled = true;
+            this.cmbPerfil.Location = new System.Drawing.Point(82, 376);
+            this.cmbPerfil.Name = "cmbPerfil";
+            this.cmbPerfil.Size = new System.Drawing.Size(268, 32);
+            this.cmbPerfil.TabIndex = 6;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(78, 83);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(67, 24);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(430, 83);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(52, 24);
+            this.lblCpf.TabIndex = 8;
+            this.lblCpf.Text = "CPF:";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(78, 178);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(62, 24);
+            this.lblLogin.TabIndex = 9;
+            this.lblLogin.Text = "Login:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.AutoSize = true;
+            this.txtEmail.Location = new System.Drawing.Point(430, 178);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(68, 24);
+            this.txtEmail.TabIndex = 10;
+            this.txtEmail.Text = "E-mail:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(78, 271);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(70, 24);
+            this.lblSenha.TabIndex = 11;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblConfSenha
+            // 
+            this.lblConfSenha.AutoSize = true;
+            this.lblConfSenha.Location = new System.Drawing.Point(430, 271);
+            this.lblConfSenha.Name = "lblConfSenha";
+            this.lblConfSenha.Size = new System.Drawing.Size(150, 24);
+            this.lblConfSenha.TabIndex = 12;
+            this.lblConfSenha.Text = "Confirma Senha:";
+            // 
+            // lblPerfil
+            // 
+            this.lblPerfil.AutoSize = true;
+            this.lblPerfil.Location = new System.Drawing.Point(78, 349);
+            this.lblPerfil.Name = "lblPerfil";
+            this.lblPerfil.Size = new System.Drawing.Size(56, 24);
+            this.lblPerfil.TabIndex = 13;
+            this.lblPerfil.Text = "Perfil:";
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.AllowDrop = true;
+            this.txtCpf.Culture = new System.Globalization.CultureInfo("pt-CH");
+            this.txtCpf.Location = new System.Drawing.Point(434, 110);
+            this.txtCpf.Mask = "000.000.000-00";
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(146, 29);
+            this.txtCpf.TabIndex = 14;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.AllowDrop = true;
+            this.txtSenha.Location = new System.Drawing.Point(82, 298);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(268, 29);
+            this.txtSenha.TabIndex = 15;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // txtConfirma
+            // 
+            this.txtConfirma.AllowDrop = true;
+            this.txtConfirma.Location = new System.Drawing.Point(434, 298);
+            this.txtConfirma.Name = "txtConfirma";
+            this.txtConfirma.Size = new System.Drawing.Size(268, 29);
+            this.txtConfirma.TabIndex = 16;
+            this.txtConfirma.UseSystemPasswordChar = true;
+            // 
+            // CadUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 544);
+            this.Controls.Add(this.txtConfirma);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.lblPerfil);
+            this.Controls.Add(this.lblConfSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.cmbPerfil);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtEmailuser);
+            this.Controls.Add(this.TxtNome);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "CadUsuario";
+            this.Text = "Cadastro de Usuário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.TextBox txtEmailuser;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.ComboBox cmbPerfil;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label txtEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblConfSenha;
+        private System.Windows.Forms.Label lblPerfil;
+        private System.Windows.Forms.MaskedTextBox txtCpf;
+        private System.Windows.Forms.MaskedTextBox txtSenha;
+        private System.Windows.Forms.MaskedTextBox txtConfirma;
+    }
+}
