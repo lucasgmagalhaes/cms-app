eebd82f7030139ce29088ea06e2a8a59aa9c6349
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-05-27 14:18:48 -0300

    Update Pessoa

--- Class-Management-System/Entities/Pessoa.cs
+++ Class-Management-System/Entities/Pessoa.cs
@@ -1,32 +1,37 @@
 ﻿namespace Class_Management_System.Entities
 {
    public class Pessoa
-    { 
-        protected string sNome;
-        protected string sEmail;
-        protected string sCPF;
-        protected int pkPessoa;
+    {
+        private string sNome;
+        private string sEmail;
+        private string sCPF;
+        private int pkPessoa;
 
-        public Pessoa( )
+        public string SNome { get => sNome; set => sNome = value; }
+        public string SEmail { get => sEmail; set => sEmail = value; }
+        public string SCPF { get => sCPF; set => sCPF = value; }
+        public int PkPessoa { get => pkPessoa; set => pkPessoa = value; }
+
+        public Pessoa()
         {
-            this.sNome = "";
-            this.sEmail = "";
-            this.sCPF = "";
-            this.pkPessoa = 0;
+            this.SNome = "";
+            this.SEmail = "";
+            this.SCPF = "";
+            this.PkPessoa = 0;
         }
         public Pessoa(string sNome, string sEmail, string sCPF, int pkPessoa)
         {
-            this.sNome = sNome;
-            this.sEmail = sEmail;
-            this.sCPF = sCPF;
-            this.pkPessoa = pkPessoa;
+            this.SNome = sNome;
+            this.SEmail = sEmail;
+            this.SCPF = sCPF;
+            this.PkPessoa = pkPessoa;
         }
 
         public Pessoa(string sNome, string sEmail, string sCPF )
         {
-            this.sNome = sNome;
-            this.sEmail = sEmail;
-            this.sCPF = sCPF; 
+            this.SNome = sNome;
+            this.SEmail = sEmail;
+            this.SCPF = sCPF; 
         } 
     }
 }
