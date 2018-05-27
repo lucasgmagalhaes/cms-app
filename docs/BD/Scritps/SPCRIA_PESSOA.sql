d5ef502812231f3aa0960f1d9abb7f315f23edb7
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-05-27 14:31:11 -0300

    Alteração para MySql

--- docs/BD/Scritps/SPCRIA_PESSOA.sql
+++ docs/BD/Scritps/SPCRIA_PESSOA.sql
@@ -1,12 +1,14 @@
 CREATE  PROC [dbo].[SPCRIA_PESSOA]
-    @sNomePessoa AS NVARCHAR(MAX),
-	@sCpfPessoa AS NVARCHAR(MAX),
-	@sEmailPessoa AS NVARCHAR(MAX) = ''
-AS 
- INSERT INTO PESSOA
- (COD_CPF ,
- NOME_PESSOA,
- EMAIL)
- VALUES(@sNomePessoa ,@sCpfPessoa ,@sEmailPessoa)
-
- DECLARE @PKPESSOA AS INTEGER = (SELECT COD_PESSOA FROM PESSOA WHERE COD_CPF = @sCpfPessoa) 
+    @sNomePessoa AS NVARCHAR(MAX) ,
+    @sCpfPessoa AS NVARCHAR(MAX) ,
+    @sEmailPessoa AS NVARCHAR(MAX) = ''
+AS
+    INSERT  INTO PESSOA
+            ( COD_CPF ,
+              NOME_PESSOA ,
+              EMAIL
+            )
+    VALUES  ( @sNomePessoa ,
+              @sCpfPessoa ,
+              @sEmailPessoa
+            );
