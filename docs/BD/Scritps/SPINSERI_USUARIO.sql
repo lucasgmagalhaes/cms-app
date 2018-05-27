1f1199a70c8d14c4595a293365393eb54c56633d
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-05-27 14:31:27 -0300

    Alteração para MySql

--- docs/BD/Scritps/SPINSERI_USUARIO.sql
+++ docs/BD/Scritps/SPINSERI_USUARIO.sql
@@ -1,20 +1,17 @@
-CREATE PROCEDURE SPINSERI_USUARIO 
+CREATE PROCEDURE SPINSERI_USUARIO
     @sLoginUS AS NVARCHAR(20) ,
-	@sSenhaUS AS NVARCHAR(20) ,
-	@pkPerfilUS AS INTEGER,
+    @sSenhaUS AS NVARCHAR(20) ,
+    @pkPerfilUS AS INTEGER ,
     @pkPessoaUS AS INTEGER
 AS
-INSERT INTO dbo.USUARIO
-        (  
-		  [LOGIN] ,
-		  SENHA,
-		  COD_PERFIL_USUARIO,
-		  COD_PESSOA
-        )
-VALUES  (  
-           @sLoginUS,
-		   @sSenhaUS,
-		   @pkPerfilUS,
-		   @pkPessoaUS
-        )
-GO	
+    INSERT  INTO dbo.USUARIO
+            ( LOGIN ,
+              SENHA ,
+              COD_PERFIL_USUARIO ,
+              COD_PESSOA
+            )
+    VALUES  ( @sLoginUS ,
+              @sSenhaUS ,
+              @pkPerfilUS ,
+              @pkPessoaUS
+            );
