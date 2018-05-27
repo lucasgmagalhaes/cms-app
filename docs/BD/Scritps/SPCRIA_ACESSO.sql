072e6017475998d4379dd0cb45fb04437d154fc6
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-05-27 14:31:08 -0300

    Alteração para MySql

--- docs/BD/Scritps/SPCRIA_ACESSO.sql
+++ docs/BD/Scritps/SPCRIA_ACESSO.sql
@@ -1,23 +1,23 @@
-CREATE  PROC [dbo].[SPCRIA_ACESSO]
-    @sNome AS NVARCHAR(MAX),
-	@sCpf AS NVARCHAR(MAX),
-	@sEmail AS NVARCHAR(MAX) = '',
-	@sLogin AS NVARCHAR(20) ,
-	@sSenha AS NVARCHAR(20) ,
-	@pkPerfil AS INTEGER
-AS  
-DECLARE @PKPESSOA AS INTEGER = (SELECT COD_PESSOA FROM PESSOA WHERE COD_CPF = @sCpf) 
-IF @PKPESSOA > 0
-BEGIN	
-EXEC SPCRIA_PESSOA @sNomePessoa = @sNome,@sCpfPessoa = @sCpf, @sEmailPessoa = @sEmail
- --INSERT INTO PESSOA
- --(COD_CPF ,
- --NOME_PESSOA,
- --EMAIL)
- --VALUES(@sNome ,@sCpf ,@sEmail)
- END  
- EXEC SPINSERI_USUARIO   @sLogin = @sLoginUS   ,
-	@sSenha  = @sSenhaUS ,
-	@pkPerfil = @pkPerfilUS,
-    @pkPessoa  =@pkPessoaUS 
- 
+CREATE PROC [dbo].[SPCRIA_ACESSO]
+    @sNome AS NVARCHAR(MAX) ,
+    @sCpf AS NVARCHAR(MAX) ,
+    @sEmail AS NVARCHAR(MAX) = '' ,
+    @sLoginUS AS NVARCHAR(20) ,
+    @sSenhaUS AS NVARCHAR(20) ,
+    @pkPerfilUS AS INTEGER
+AS
+    DECLARE @PKPESSOA AS INTEGER = ( SELECT COD_PESSOA
+                                     FROM   PESSOA
+                                     WHERE  COD_CPF = @sCpf
+                                   );
+    IF @PKPESSOA = 0
+        BEGIN
+            EXEC SPCRIA_PESSOA @sNomePessoa = @sNome, @sCpfPessoa = @sCpf,
+                @sEmailPessoa = @sEmail;
+        END
+    SET @PKPESSOA = ( SELECT    COD_PESSOA
+                      FROM      PESSOA
+                      WHERE     COD_CPF = @sCpf
+                    );
+    EXEC SPINSERI_USUARIO @sLogin = @sLoginUS, @sSenha = @sSenhaUS,
+        @pkPerfil = @pkPerfilUS, @pkPessoa = @PKPESSOA;
