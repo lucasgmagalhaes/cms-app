218cfbe4c2234d4f63e564d22f6830d0aa3f5dea
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-05-29 00:07:49 -0300

    Update BuscaUsuario
    Criada funções e eventos para a pesquisa de usuário.

--- Class-Management-System/Forms/BuscaUsuario.Designer.cs
+++ Class-Management-System/Forms/BuscaUsuario.Designer.cs
@@ -28,58 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.lblPesquisa = new System.Windows.Forms.Label();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.BtnPesquisar = new System.Windows.Forms.Button();
+            this.dtgPesquisa = new System.Windows.Forms.DataGridView();
+            this.CmbFiltro = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPesquisa)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblPesquisa
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(121, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblPesquisa.AutoSize = true;
+            this.lblPesquisa.Location = new System.Drawing.Point(165, 37);
+            this.lblPesquisa.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPesquisa.Name = "lblPesquisa";
+            this.lblPesquisa.Size = new System.Drawing.Size(92, 24);
+            this.lblPesquisa.TabIndex = 0;
+            this.lblPesquisa.Text = "Pesquisa:";
             // 
-            // textBox1
+            // txtPesquisa
             // 
-            this.textBox1.Location = new System.Drawing.Point(124, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(449, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesquisa.Location = new System.Drawing.Point(170, 66);
+            this.txtPesquisa.Margin = new System.Windows.Forms.Padding(6);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(667, 38);
+            this.txtPesquisa.TabIndex = 1;
             // 
-            // button1
+            // BtnPesquisar
             // 
-            this.button1.Location = new System.Drawing.Point(579, 34);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnPesquisar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BtnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPesquisar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnPesquisar.Location = new System.Drawing.Point(1002, 66);
+            this.BtnPesquisar.Margin = new System.Windows.Forms.Padding(6);
+            this.BtnPesquisar.Name = "BtnPesquisar";
+            this.BtnPesquisar.Size = new System.Drawing.Size(138, 39);
+            this.BtnPesquisar.TabIndex = 2;
+            this.BtnPesquisar.Text = "PESQUISAR";
+            this.BtnPesquisar.UseVisualStyleBackColor = false;
+            this.BtnPesquisar.Click += new System.EventHandler(this.BtnPesquisar_Click);
             // 
-            // dataGridView1
+            // dtgPesquisa
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(124, 62);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(530, 340);
-            this.dataGridView1.TabIndex = 3;
+            this.dtgPesquisa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgPesquisa.Location = new System.Drawing.Point(170, 114);
+            this.dtgPesquisa.Margin = new System.Windows.Forms.Padding(6);
+            this.dtgPesquisa.Name = "dtgPesquisa";
+            this.dtgPesquisa.Size = new System.Drawing.Size(972, 628);
+            this.dtgPesquisa.TabIndex = 3;
+            this.dtgPesquisa.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgPesquisa_CellContentClick);
+            // 
+            // CmbFiltro
+            // 
+            this.CmbFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmbFiltro.FormattingEnabled = true;
+            this.CmbFiltro.Items.AddRange(new object[] {
+            "ID",
+            "LOGIN",
+            "NOME",
+            "CPF"});
+            this.CmbFiltro.Location = new System.Drawing.Point(846, 66);
+            this.CmbFiltro.Name = "CmbFiltro";
+            this.CmbFiltro.Size = new System.Drawing.Size(147, 39);
+            this.CmbFiltro.TabIndex = 4;
             // 
             // BuscaUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1392, 809);
+            this.Controls.Add(this.CmbFiltro);
+            this.Controls.Add(this.dtgPesquisa);
+            this.Controls.Add(this.BtnPesquisar);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.lblPesquisa);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "BuscaUsuario";
             this.Text = "BuscaUsuario";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPesquisa)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,9 +116,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblPesquisa;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Button BtnPesquisar;
+        private System.Windows.Forms.DataGridView dtgPesquisa;
+        private System.Windows.Forms.ComboBox CmbFiltro;
     }
 }
--- Class-Management-System/Forms/BuscaUsuario.cs
+++ Class-Management-System/Forms/BuscaUsuario.cs
@@ -1,20 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Class_Management_System.Services;
+using Class_Management_System.Utils;
+using Class_Management_System.Entities;
 
 namespace Class_Management_System.Forms
 {
     public partial class BuscaUsuario : Form
     {
+        private readonly IDataBaseService dbService;
+        DataTable dtbPesquisa = new DataTable();
         public BuscaUsuario()
         {
             InitializeComponent();
+            this.dbService = DependencyFactory.Resolve<IDataBaseService>();
+            MontaDtb();
+        }
+
+        private void BtnPesquisar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dtbPesquisa = GeraDadosPesquisa();
+                dtgPesquisa.DataSource = dtbPesquisa;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro - BtnPesquisar_Click " + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+        public void MontaDtb()
+        {
+            try
+            {
+                dtbPesquisa.Columns.Add("ID", Type.GetType("System.Int32"));
+                dtbPesquisa.Columns.Add("LOGIN", Type.GetType("System.String"));
+                dtbPesquisa.Columns.Add("NOME", Type.GetType("System.String"));
+                dtbPesquisa.Columns.Add("CPF", Type.GetType("System.String"));
+                dtbPesquisa.Columns.Add("E-MAIL", Type.GetType("System.String"));
+                dtbPesquisa.Columns.Add("PERFIL", Type.GetType("System.String"));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erro - MontaDtb " + e.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        public DataTable GeraDadosPesquisa()
+        {
+            try
+            {
+                string sSql = " EXEC SPCONSULTA_USUARIO ";
+                if (string.IsNullOrEmpty(txtPesquisa.Text) == false)
+                {
+                    switch (CmbFiltro.SelectedItem.ToString())
+                    {
+                        case "ID":
+                            sSql += " @pkUsuario = '" + txtPesquisa.Text + "'";
+                            break;
+                        case "LOGIN":
+                            sSql += " @sLogin = '" + txtPesquisa.Text + "'";
+                            break;
+                        case "NOME":
+                            sSql += " @sNomePessoa = '" + txtPesquisa.Text + "'";
+                            break;
+                        case "CPF":
+                            sSql += " @sCpf = '" + txtPesquisa.Text + "'";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                return dbService.BuscaDados(sSql);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erro - MontaGrade " + e.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+        private void DtgPesquisa_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 3 && e.Value != null)
+                {
+                    Double.TryParse(e.Value.ToString(), out double d);
+                    e.Value = d.ToString(@"000\.000\.000-000");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro - DtgPesquisa_CellFormatting " + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+
+        }
+
+        private void DtgPesquisa_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                object value = dtgPesquisa.Rows[e.RowIndex].Cells[0].Value;
+                if (value is DBNull) { return; }
+                int pkUsuario = (int)value;
+                CadUsuario cadastro = new CadUsuario(pkUsuario);
+                cadastro.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro - DtgPesquisa_CellContentClick " + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
         }
     }
 }
