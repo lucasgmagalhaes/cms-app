395bb744fd8cd1bb380a30d8216e9060bb4a2b7f
hkirschke <33073460+hkirschke@users.noreply.github.com>
2018-05-31 11:21:05 -0300

    Correção para MySql

--- docs/BD/Scritps/SPCONSULTA_USUARIO.sql
+++ docs/BD/Scritps/SPCONSULTA_USUARIO.sql
@@ -1,12 +1,12 @@
-CREATE  PROC [dbo].[SPCONSULTA_USUARIO]
-    @pkUsuario AS NVARCHAR(4) = '0' ,
-    @sLogin AS NVARCHAR(20) = '' ,
-    @sNomePessoa AS NVARCHAR(MAX) = '' ,
-    @sCpf AS NVARCHAR(MAX) = ''
-AS
-    DECLARE @sSql AS NVARCHAR(MAX) = '';
-    SET @sSql = 'SELECT  U.COD_USUARIO ID ,
-            U.LOGIN [LOGIN] ,
+DELIMITER $$
+CREATE PROCEDURE  SPCONSULTA_USUARIO (  IN sLogin VARCHAR(20) ,   IN pkUsuario VARCHAR(4) ,    IN sNomePessoa VARCHAR(100) ,  IN sCpf VARCHAR(100)   )
+BEGIN
+	 SET pkUsuario = IFNULL(pkUsuario, '0');
+	 SET sLogin = IFNULL(sLogin, '');
+	 SET sNomePessoa = IFNULL(sNomePessoa, '');
+	 SET sCpf = IFNULL(sCpf, '');  
+	 SET @sSql = 'SELECT  U.COD_USUARIO ID ,
+            U.LOGIN LOGIN ,
             PU.DSC_PERFIL_USUARIO PERFIL ,
             P.NOME_PESSOA,
  	        P.COD_CPF,
@@ -14,24 +14,23 @@ AS
 			P.COD_PESSOA,
 			PU.COD_PERFIL_USUARIO,
 			U.SENHA
-    FROM    dbo.USUARIO U
-            LEFT JOIN dbo.PERFIL_USUARIO PU ON PU.COD_PERFIL_USUARIO = U.COD_PERFIL_USUARIO
-            LEFT JOIN dbo.PESSOA P ON P.COD_PESSOA = U.COD_PESSOA WHERE 1=1 '
-    IF @pkUsuario <> '0'
-        BEGIN
-            SET @sSql += ' AND U.COD_USUARIO = ' + @pkUsuario;
-        END
-    IF @sLogin <> ''
-        BEGIN
-            SET @sSql += ' AND U.NOME_USUARIO LIKE ''' + @pkUsuario + '%''';
-        END
-    IF @sNomePessoa <> ''
-        BEGIN
-            SET @sSql += ' AND P.NOME_PESSOA LIKE ''' + @sNomePessoa + '%''';
-        END
-    IF @sCpf <> ''
-        BEGIN
-            SET @sSql += ' AND P.COD_CPF = ''' + @sCpf + '''';
-        END
-    EXEC ( @sSql );
-GO
+    FROM    USUARIO U
+            LEFT JOIN PERFIL_USUARIO PU ON PU.COD_PERFIL_USUARIO = U.COD_PERFIL_USUARIO
+            LEFT JOIN PESSOA P ON P.COD_PESSOA = U.COD_PESSOA WHERE 1=1 ';
+    IF (pkUsuario <> '0')  THEN 
+            SET @sSql = @sSql + ' AND U.COD_USUARIO = ' + pkUsuario; 	
+         END IF;
+    IF (sLogin <> '' ) THEN 
+            SET @sSql = @sSql + ' AND U.NOME_USUARIO LIKE ''' + sLogin + '%''';
+         END IF;
+    IF (sNomePessoa <> '')  THEN 
+            SET @sSql = @sSql + ' AND P.NOME_PESSOA LIKE ''' + sNomePessoa + '%''';
+         END IF;
+    IF (sCpf <> '') THEN 
+            SET @sSql  = @sSql + ' AND P.COD_CPF = ''' + sCpf + '''';
+            END IF;
+	PREPARE myquery FROM @sSql;
+    EXECUTE myquery; 
+	DEALLOCATE PREPARE myquery;
+END $$
+DELIMITER ;
